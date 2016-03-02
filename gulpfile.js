c2e6b6135338a592d4bd498898a4da1a704c3c15
Martin Kearn <martin.kearn@microsoft.com>
2016-03-02 09:30:37 +0000

    Initial copy of website
    From Web-Performance repo

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp'),
+	cssmin = require('gulp-minify-css'),
+	jsmin = require('gulp-uglify'),
+  concat = require('gulp-concat'),
+  gzip = require('gulp-gzip');
+
+gulp.task('task-cssmin', function() {
+  gulp.src('css/*.css')
+  .pipe(cssmin())
+  .pipe(concat('bundle.css'))
+  .pipe(gulp.dest("wwwroot"))
+});
+
+gulp.task('task-jsmin', function() {
+  gulp.src('js/*.js')
+  .pipe(jsmin())
+  .pipe(concat('bundle.js')) 
+  .pipe(gulp.dest("wwwroot"))
+});
+
+gulp.task("default", ['task-cssmin','task-jsmin'])
